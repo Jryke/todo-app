2872efff83f28d74592a3e549d41d0e9ca92c57e
Jryke <rykerson@gmail.com>
2020-04-01 12:49:46 -0400

    JS refactored and cleaned up

--- script.js
+++ script.js
@@ -9,21 +9,33 @@ const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
-// function to get number of unchecked items and render to unchecked-count
-const getUncheckedCount = () => {
+// function to render number of items to item-count
+const renderItemCount = () => {
+  // get number of todo items
+  const count = document.getElementsByClassName(classNames.TODO_ITEM).length
+  // render 
+  itemCountSpan.innerHTML = count
+}
+
+// function to render number of unchecked items to unchecked-count
+const renderUncheckedCount = () => {
   // get array of checkboxes, filter out checked boxes, set value to length of array
   const uncheckedCountValue = [...document.getElementsByClassName(classNames.TODO_CHECKBOX)].filter(item => !item.checked).length
   // render number of unchecked to unchecked-count
   uncheckedCountSpan.innerHTML = uncheckedCountValue
-}  
+}
 
 // function to remove todo from list
 const deleteItem = (e) => {
   // identify button element parent and remove it
   e.target.parentElement.remove()
+  // rerender item-count and unchecked-count
+  renderItemCount()
+  renderUncheckedCount()
 }
 
-function newTodo() {
+const newTodo = () => {
+  // prompt modal to enter task value
   let todo = prompt('enter new to do task:')
 
   // conditional to check for todo value
@@ -35,18 +47,18 @@ function newTodo() {
     // create/append todo-checkbox to todo-list div
     const checkbox = document.createElement('input')
     checkbox.type = 'checkbox'
-    checkbox.onchange = getUncheckedCount
+    checkbox.onchange = renderUncheckedCount
     checkbox.classList.add(classNames.TODO_CHECKBOX)
     newTodoDiv.appendChild(checkbox)
 
     // create/append todo-text to todo-list div
-    let textEl = document.createElement('span')
+    const textEl = document.createElement('span')
     textEl.classList.add(classNames.TODO_TEXT)
     textEl.innerHTML = todo
     newTodoDiv.appendChild(textEl)
 
     // append todo-delete button to todo-list div
-    let deleteButton = document.createElement('button')
+    const deleteButton = document.createElement('button')
     deleteButton.type = 'button'
     deleteButton.onclick = deleteItem
     deleteButton.innerHTML = 'Delete'
@@ -56,17 +68,11 @@ function newTodo() {
     // append todo-container to todo-list
     list.appendChild(newTodoDiv)
 
-    // get number of list items and render to item-count
-    const count = list.childNodes.length
-    itemCountSpan.innerHTML = count
-
-    // render unchecked count to update when new item is added
-    getUncheckedCount()
+    // render item-count and unchecked-count when new item is added
+    renderItemCount()
+    renderUncheckedCount()
     
   } else {
     alert('No todo has been entered!  Please try again.')
   }
-  
-  
-
 }
