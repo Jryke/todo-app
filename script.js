278872125ad13f405a95789b010517b181cd02ff
Jryke <rykerson@gmail.com>
2020-04-01 10:16:29 -0400

    prompt to set new todo, rendering as li to todo-list  with a checkbox and delete button

--- script.js
+++ script.js
@@ -10,5 +10,38 @@ const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
 function newTodo() {
-  alert('New TODO button clicked!')
+  let todo = prompt('enter new to do task:')
+
+  // conditional to check for todo value
+  if (todo !== null && todo !== '') {
+    // create todo-container div
+    const newTodoDiv = document.createElement('li')
+    newTodoDiv.classList.add(classNames.TODO_ITEM)
+
+    // create/append todo-checkbox to todo-list div
+    const checkbox = document.createElement('input')
+    checkbox.type = 'checkbox'
+    checkbox.classList.add(classNames.TODO_CHECKBOX)
+    newTodoDiv.appendChild(checkbox)
+
+    // create/append todo-text to todo-list div
+    let textEl = document.createElement('span')
+    textEl.classList.add(classNames.TODO_TEXT)
+    textEl.innerHTML = todo
+    newTodoDiv.appendChild(textEl)
+
+    // append todo-delete button to todo-list div
+    let deleteButton = document.createElement('button')
+    deleteButton.type = 'button'
+    deleteButton.innerHTML = 'Delete'
+    deleteButton.classList.add(classNames.TODO_DELETE)
+    newTodoDiv.appendChild(deleteButton)
+
+    // append todo-container to todo-list
+    list.appendChild(newTodoDiv)
+
+  } else {
+    alert('No todo has been entered!  Please try again.')
+  }
+  
 }
